15ad75d739dcd9bd99df9477f83e1c27ce265f33
Shalin Scupham <shalin.scupham@gmail.com>
2018-01-21 04:18:16 -0500

    Finally have basic paperscript running...

--- test.js
+++ test.js
@@ -1,38 +1,13 @@
-let Square = Base.extend({
-  initialize: function(position, size, maxSpeed) {
-    this.maxSpeed = maxSpeed;
-    this.position = position.clone();
-    this.size = size.clone();
-    this.vector = (Point.random() * 2 - 1);
-    this.drawBox(position, size);
-  },
-  drawBox: function(position, size) {
-    this.sq = new Path.Rectangle({
-      center: position,
-      size: size,
-      strokeColor: 'black'
-    });
-  },
-  run: function(squares) {
-    this.position += this.vector;
-    this.sq.position = this.position;
-  },
-
-})
-
-
-function onFrame(event) {
-  for (var i = 0; i < squares.length; i++) {
-    squares[i].run(squares);
-  }
-}
-
-const squares = [];
+var circles = [];
 
 function onKeyDown(event) {
-    let position = Point.random() * view.size;
-    let side = 300 * Math.random();
-    let size = new Size(side);
-    let maxSpeed = 10;
-    new Square(position, size, maxSpeed)
-};
+  var point = Point.random() * view.size
+  var newCircle = new Path.Circle(point, 500);
+  newCircle.strokeColor = 'black';
+  circles.push(newCircle);
+}
+function onFrame(event) {
+  for (i = 0; i < circles.length; i++) {
+    circles[i].scale(.9);
+  }
+}
